f3b27ba0abeb37965179aa9175133c908263d3a5
yblii <yb.li0919@gmail.com>
2025-06-15 22:13:11 -0700

    Implemented basic boid behavior.

--- modules/boid.js
+++ modules/boid.js
@@ -1,10 +1,10 @@
 export class Boid {
-    constructor(x, y) {
+    constructor(x, y, speed, p5) {
         this.x = x;
         this.y = y;
-        
-        this.dx = 0.5;
-        this.dy = 0.5;
+        this.speed = speed;
+        this.p5 = p5;
+        this.velocity = p5.createVector(0.5, 0.5);
 
         this.parentNode = undefined;
     }
@@ -14,7 +14,63 @@ export class Boid {
     }
 
     update(neighbors) {
-        this.x += this.dx;
-        this.y += this.dy;
+        this.steerToCenter(neighbors);
+        this.align(neighbors);
+
+        this.velocity.setMag(this.speed);
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+
+    steerToCenter(neighbors) {
+        // average position of neighbors
+        let centerX = 0;
+        let centerY = 0;
+
+        for(const boid of neighbors) {
+            centerX += boid.x;
+            centerY += boid.y;
+        }
+
+        centerX /= neighbors.length;
+        centerY /= neighbors.length;
+
+        const diff = this.p5.createVector(centerX - this.x, centerY - this.y);
+
+        this.velocity.add(diff.mult(0.005));
+    }
+
+    align(neighbors) {
+        const aveVelocity = this.p5.createVector(0, 0);
+
+        for(const boid of neighbors) {
+            aveVelocity.add(boid.velocity);
+        }
+
+        aveVelocity.mult(1 / neighbors.length);
+        
+        aveVelocity.sub(this.velocity);
+        this.velocity.add(aveVelocity.mult(0.05));
+    }
+
+    separate(neighbors) {
+        for(const boid of neighbors) {
+            if(this.distBetween(boid) < 20) {
+                const diff = this.p5.createVector(boid.x - this.x, boid.y - this.y);
+                this.velocity.sub(diff.mult(0.5));
+            }
+        }
+    }
+
+    distBetween(other) {
+        return (other.x - this.x) * (other.x - this.x) + (other.y - this.y) * (other.y - this.y);
+    }
+
+    debug(sketch) {
+        sketch.fill('red');
+        this.p5.line(this.x, this.y, this.x + this.velocity.x * 5, this.y + this.velocity.y * 5);
+        sketch.fill('white');
+        
     }
 }
--- modules/sketch.js
+++ modules/sketch.js
@@ -5,14 +5,14 @@ import { Boid } from "./boid.js";
 const s = ( sketch ) => {
   const WIDTH = 1000;
   const HEIGHT = 1000;
-  const PARTICLE_COUNT = 100;
+  const PARTICLE_COUNT = 50;
   const SPLITTING_FACTOR = 3;
 
   const PARTICLES = [];
   const TREE = new QuadTree(SPLITTING_FACTOR, new Bound(0, WIDTH, 0, HEIGHT));
 
   for(let i = 0; i < PARTICLE_COUNT; i++) {
-    const boid = new Boid(Math.random() * 400, Math.random() * 400);
+    const boid = new Boid(Math.random() * 400 + 200, Math.random() * 400 + 200, 2, sketch);
     TREE.add(boid);
     PARTICLES.push(boid);
   };
@@ -27,13 +27,13 @@ const s = ( sketch ) => {
     sketch.background(0);
     sketch.fill(255);
     for(const boid of PARTICLES) {
-      boid.update();
+      boid.update(TREE.getNeighbors(boid));
       if(!boid.parentNode.bound.contains(boid.position)) {
         TREE.update(boid);
       }
       sketch.circle(boid.position.x, boid.position.y, 10);
+      boid.debug(sketch);
     }
-    TREE.debug(sketch);
   };
 };
 
